2e8764ef8432cf526252bb8b020456b70540d267
Christian Gram Kalhauge <kalhauge@cs.ucla.edu>
2020-04-30 19:25:23 +0200

    A small fix

--- default.nix
+++ default.nix
@@ -11,7 +11,7 @@ let
 in
   haskellPackages.developPackage {
     root = pkgs.lib.cleanSourceWith 
-      { filter = path: type: baseNameOf path != ".nix";
+      { filter = path: type: ! (pkgs.lib.hasSuffix ".nix" path);
         src = pkgs.lib.cleanSource ./.;
       };
     name = "javaq";
