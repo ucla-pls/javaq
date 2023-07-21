36359e44dfcf992f117af0e15f3f08dc9e98698c
Christian Gram Kalhauge <chrg@dtu.dk>
2023-07-21 15:05:32 +0200

    Upgrade javaq

--- flake.nix
+++ flake.nix
@@ -1,45 +1,41 @@
 {
   description = "JavaQ - a jvm decompiler";
 
-  inputs = 
+  inputs =
     {
-      nixpkgs.url = "github:NixOS/nixpkgs/21.05";
+      nixpkgs.url = "github:NixOS/nixpkgs/23.05";
       flake-utils.url = "github:numtide/flake-utils";
-      hspec-hedgehog = { 
-        url = "https://hackage.haskell.org/package/hspec-hedgehog-0.0.1.2/hspec-hedgehog-0.0.1.2.tar.gz";
-        flake = false;
-      };
-      jvm-binary = { 
-        url = "https://hackage.haskell.org/package/jvm-binary-0.10.0/jvm-binary-0.10.0.tar.gz";
-        flake = false;
-      };
-      jvmhs = { 
-        url = "github:ucla-pls/jvmhs";
-        flake = false;
-      };
+      jvm-binary.url = github:ucla-pls/jvm-binary;
+      jvmhs.url = github:ucla-pls/jvmhs;
     };
 
-  outputs = { self, nixpkgs, flake-utils 
-    , hspec-hedgehog, jvmhs, jvm-binary }:
-    flake-utils.lib.eachSystem [ "x86_64-darwin" ] (system:
-      let
-        pkgs = (import nixpkgs { inherit system; });
-        haskellPackages = pkgs.haskellPackages;
-        project = returnShellEnv:
-          haskellPackages.developPackage {
-            inherit returnShellEnv;
-            root = self;
-            name = "javaq";
-            source-overrides = {
-              inherit hspec-hedgehog jvmhs jvm-binary;
-            };
-            overrides = hsuper: hself: { };
-            modifier = drv:
-              pkgs.haskell.lib.addBuildTools drv
-              (with haskellPackages; [ cabal-install ghcid ]);
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , jvmhs
+    , jvm-binary
+    }:
+    flake-utils.lib.eachSystem [ "x86_64-darwin" "x86_64-linux" ] (system:
+    let
+      pkgs = (import nixpkgs { inherit system; });
+      haskellPackages = pkgs.haskellPackages;
+      project = returnShellEnv:
+        haskellPackages.developPackage {
+          inherit returnShellEnv;
+          root = self;
+          name = "javaq";
+          source-overrides = {
+            inherit jvmhs jvm-binary;
           };
-      in {
-        defaultPackage = project false;
-        devShell = project true;
-      });
+          overrides = hsuper: hself: { };
+          modifier = drv:
+            pkgs.haskell.lib.addBuildTools drv
+              (with haskellPackages; [ cabal-install ghcid haskell-language-server hpack fourmolu ]);
+        };
+    in
+    {
+      defaultPackage = project false;
+      devShell = project true;
+    });
 }
